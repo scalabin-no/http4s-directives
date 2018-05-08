d87d5796326dab7d34cff381c2f3c7fc1587ff40
Erlend Hamnaberg <erlend@hamnaberg.net>
2018-05-08 19:10:42 +0200

    Server sent events

---
+++ src/test/scala/no/scalabin/http4s/directives/SSEApp.scala
@@ -0,0 +1,41 @@
+package no.scalabin.http4s.directives
+
+
+import cats.effect.IO
+import fs2._
+import org.http4s._
+import org.http4s.server.blaze._
+import org.http4s.dsl.io._
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object SSEApp extends StreamApp[IO] {
+  override def stream(args: List[String], requestShutdown: IO[Unit]): fs2.Stream[IO, StreamApp.ExitCode] = {
+    val directives = Directives[IO]
+    import directives._
+    import ops._
+
+    val pathMapping = Plan[IO].PathMapping
+
+    val service = HttpService[IO] {
+      pathMapping {
+        case _ => for {
+          _ <- Method.GET
+          res <- Ok(events.map(e => ServerSentEvent(e.toString))).successValue
+        } yield {
+          res
+        }
+      }
+    }
+
+    BlazeBuilder[IO].bindLocal(8080).mountService(service).serve
+  }
+
+  def events: Stream[IO, Event] = {
+    val seconds = Scheduler[IO](2).flatMap(_.awakeEvery[IO](1.second))
+    seconds.take(10).map(_ => Event("event"))
+  }
+
+
+  case class Event(name: String)
+}
