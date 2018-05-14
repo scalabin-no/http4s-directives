1896b34d946d4d7c0da302400d30b626614a021f
Erlend Hamnaberg <erlend@hamnaberg.net>
2018-05-14 20:21:33 +0200

    When can now also deal with F wrapped values.

--- src/main/scala/no/scalabin/http4s/directives/when.scala
+++ src/main/scala/no/scalabin/http4s/directives/when.scala
@@ -11,4 +11,10 @@ case class when[F[+_]: Sync, R](f:PartialFunction[Request[F], R]) extends Reques
       case Some(r) => Directive.success(r)
       case None => Directive.failure(fail)
     })
+
+  def orElseF[L](fail: => F[L]): Directive[F, L, R] =
+    request.apply.flatMap(req => f.lift(req) match {
+      case Some(r) => Directive.success(r)
+      case None => Directive.failureF(fail)
+    })
 }
