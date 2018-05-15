94cc7cc9930234cfc847700586445b080978c094
Erlend Hamnaberg <erlend@hamnaberg.net>
2018-05-15 08:31:54 +0200

    orElseF to orElse

--- src/main/scala/no/scalabin/http4s/directives/when.scala
+++ src/main/scala/no/scalabin/http4s/directives/when.scala
@@ -6,13 +6,7 @@ import org.http4s.Request
 import scala.language.higherKinds
 
 case class when[F[+_]: Sync, R](f:PartialFunction[Request[F], R]) {
-  def orElse[L](fail: => L): Directive[F, L, R] =
-    Directive.request.flatMap(req => f.lift(req) match {
-      case Some(r) => Directive.success(r)
-      case None => Directive.failure(fail)
-    })
-
-  def orElseF[L](fail: => F[L]): Directive[F, L, R] =
+  def orElse[L](fail: => F[L]): Directive[F, L, R] =
     Directive.request.flatMap(req => f.lift(req) match {
       case Some(r) => Directive.success(r)
       case None => Directive.failureF(fail)
