89e07ac78667d57b77e92a23857f533901497095
Erlend Hamnaberg <erlend@hamnaberg.net>
2019-12-02 11:28:24 +0100

    Do not required Sync for Directive routes

--- src/main/scala/no/scalabin/http4s/directives/DirectiveRoutes.scala
+++ src/main/scala/no/scalabin/http4s/directives/DirectiveRoutes.scala
@@ -1,17 +1,17 @@
 package no.scalabin.http4s.directives
 
 import cats.syntax.functor._
-import cats.effect.Sync
-import org.http4s.{AuthedRequest, AuthedRoutes, HttpRoutes, Request, Response}
+import cats.{Defer, Applicative}
+import org.http4s.{ContextRequest, AuthedRoutes, HttpRoutes, Request, Response}
 
 object DirectiveRoutes {
-  def apply[F[_]: Sync](pf: PartialFunction[Request[F], Directive[F, Response[F]]]): HttpRoutes[F] = HttpRoutes.of[F] {
+  def apply[F[_]: Defer: Applicative](pf: PartialFunction[Request[F], Directive[F, Response[F]]]): HttpRoutes[F] = HttpRoutes.of[F] {
     case req if pf.isDefinedAt(req) => pf(req).run(req).map(_.response)
   }
 }
 
 object AuthedDirectiveRoutes {
-  def apply[A, F[_]: Sync](pf: PartialFunction[AuthedRequest[F, A], Directive[F, Response[F]]]): AuthedRoutes[A, F] =
+  def apply[A, F[_]: Defer: Applicative](pf: PartialFunction[ContextRequest[F, A], Directive[F, Response[F]]]): AuthedRoutes[A, F] =
     AuthedRoutes.of[A, F] {
       case req if pf.isDefinedAt(req) => pf(req).run(req.req).map(_.response)
     }
